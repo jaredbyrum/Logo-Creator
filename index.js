d50199e5f81f00bdcd4e12c62771761656eda430
Jared Byrum <jaredbyrum@gmail.com>
2023-11-06 18:16:09 -0500

    app works, added example svgs, added comments to code for sources

--- index.js
+++ index.js
@@ -32,15 +32,22 @@ function writeSvg(answers) {
   let svgString = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
   svgString += `<g>`;
   svgString += `${answers.shape}`;
+  let shape;
   switch(answers.shape) {
-    case 'Triangle':  
-      svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}"/>`;
+    case 'Triangle':
+      shape = new Triangle();
+      shape.setColor(answers.shapeColor)  
+      svgString += shape.render();
       break;
     case 'Square': 
-      svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeColor}"/>`
+      shape = new Square();
+      shape.setColor(answers.shapeColor)  
+      svgString += shape.render();
       break;
     case 'Circle':
-      svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeColor}"/>`
+      shape = new Circle();
+      shape.setColor(answers.shapeColor)  
+      svgString += shape.render();
       break;    
   }
   svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.color}">${answers.text}</text>`;
